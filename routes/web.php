84fd62ec00d2046368219fd367ba6f8961f7b1d4
Norio Mastsumoto <matsumoto@donow.jp>
2017-09-21 14:01:07 +0000

    ルーティングのパターンをいくつか登録

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,97 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//直打ち
+Route::get('hello', function (){
+    return '<html><body><h1>hello</h1><p>this is sample page</p></body></html>';
+});
+
+$html = <<<EOF
+<html>
+<head>
+<title>本日は晴天なり</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1{font-size:100pt; text-align:right; color:#eee;
+  margin:-40px 0px -50px 0;
+}
+</style>
+</head>
+<body>
+<h1>Hello</h1>
+<p>this is sample page.</p>
+<p>サンプルページですよ!</p>
+</body>
+</html>
+EOF;
+
+//直うちヒアドキュメント
+Route::get('hello2', function () use ($html) {
+    return $html;
+});
+
+//直打ち+パラメータ
+Route::get('hello3/{msg}', function ($msg) {
+
+$html = <<<EOF
+<html>
+<head>
+<title>本日は晴天なり</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1{font-size:100pt; text-align:right; color:#eee;
+  margin:-40px 0px -50px 0;
+}
+</style>
+</head>
+<body>
+<h1>Hello</h1>
+<p>{$msg}</p>
+<p>サンプルページですよ!</p>
+</body>
+</html>
+EOF;
+    return $html;
+
+});
+
+//直打ち+パラメータ2つ
+Route::get('hello4/{id}/{pass}', function ($id, $pass) {
+
+$html = <<<EOF
+id:{$id}
+pass:{$pass}
+EOF;
+    return $html;
+
+});
+
+//直打ち+任意パラメータ(パラメーターあってもなくてもOK)
+//通常はないとエラーになる
+Route::get('hello5/{msg?}', function ($msg = 'no message') {
+
+$html = <<<EOF
+<html>
+<head>
+<title>本日は晴天なり</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1{font-size:100pt; text-align:right; color:#eee;
+  margin:-40px 0px -50px 0;
+}
+</style>
+</head>
+<body>
+<h1>Hello</h1>
+<p>{$msg}</p>
+<p>サンプルページですよ!</p>
+</body>
+</html>
+EOF;
+    return $html;
+
+});
+
+
+
 Route::resource('posts', 'PostsController');
